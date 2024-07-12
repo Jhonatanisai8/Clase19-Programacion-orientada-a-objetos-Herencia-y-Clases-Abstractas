b6e96c07d1212fc72c341d9c3e2b3b247512d300
jhonatan <ojedasnachezjhonatan@gmail.com>
2024-07-12 10:23:48 -0500

    clase TextAreaForm: clase hija de ElementoForm

---
+++ src/org/jhonatan/pooclasesastractas/form/elementos/TextAreaForm.java
@@ -0,0 +1,10 @@
+package org.jhonatan.pooclasesastractas.form.elementos;
+
+public class TextAreaForm extends ElementoForm {
+
+    @Override
+    public String dibujarHtml() {
+        return null;
+    }
+
+}
