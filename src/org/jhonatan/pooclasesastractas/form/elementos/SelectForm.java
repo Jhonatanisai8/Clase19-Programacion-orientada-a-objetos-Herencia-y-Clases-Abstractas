429c7099270dfbdfc70e518d0ae2be41de97a9ff
jhonatan <ojedasnachezjhonatan@gmail.com>
2024-07-12 10:38:10 -0500

    clase SelectForm : clase hija de ElementoForm

---
+++ src/org/jhonatan/pooclasesastractas/form/elementos/SelectForm.java
@@ -0,0 +1,12 @@
+package org.jhonatan.pooclasesastractas.form.elementos;
+
+public class SelectForm extends ElementoForm {
+
+    @Override
+    public String dibujarHtml() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    
+}
