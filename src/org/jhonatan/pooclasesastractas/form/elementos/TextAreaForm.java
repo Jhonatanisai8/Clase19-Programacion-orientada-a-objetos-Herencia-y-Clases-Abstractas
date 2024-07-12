ab5204ea99670afec30c18a1f107026d61dba6f5
jhonatan <ojedasnachezjhonatan@gmail.com>
2024-07-12 10:24:57 -0500

    clase TextAreaForm: atributos, getters y setters de ellos

--- src/org/jhonatan/pooclasesastractas/form/elementos/TextAreaForm.java
+++ src/org/jhonatan/pooclasesastractas/form/elementos/TextAreaForm.java
@@ -2,9 +2,29 @@ package org.jhonatan.pooclasesastractas.form.elementos;
 
 public class TextAreaForm extends ElementoForm {
 
+    // atributos
+    private int filas;
+    private int columnas;
+
     @Override
     public String dibujarHtml() {
         return null;
+    }
+
+    public int getFilas() {
+        return filas;
+    }
+
+    public void setFilas(int filas) {
+        this.filas = filas;
+    }
+
+    public int getColumnas() {
+        return columnas;
+    }
+
+    public void setColumnas(int columnas) {
+        this.columnas = columnas;
     }
 
 }
