1745d25d9f2042616327cd5030797802e595136f
jhonatan <ojedasnachezjhonatan@gmail.com>
2024-07-12 11:11:41 -0500

    clase main EjemploForm: forma de una crear una clase anomina

--- src/org/jhonatan/pooclasesastractas/form/EjemploForm.java
+++ src/org/jhonatan/pooclasesastractas/form/EjemploForm.java
@@ -1,5 +1,15 @@
 package org.jhonatan.pooclasesastractas.form;
 
+import org.jhonatan.pooclasesastractas.form.elementos.ElementoForm;
+
 public class EjemploForm {
+    public static void main(String[] args) {
+        ElementoForm el = new ElementoForm() {
+            @Override
+            public String dibujarHtml() {
+                return null;
+            }
+        };
+    }
 
 }
