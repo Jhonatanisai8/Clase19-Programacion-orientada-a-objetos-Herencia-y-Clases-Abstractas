80845e396f3cba7c6beacaf04cfda277d62ced55
jhonatan <ojedasnachezjhonatan@gmail.com>
2024-07-12 11:20:45 -0500

    clase main EjemploForm: creando objetos y estableciendo sus valores

--- src/org/jhonatan/pooclasesastractas/form/EjemploForm.java
+++ src/org/jhonatan/pooclasesastractas/form/EjemploForm.java
@@ -1,9 +1,46 @@
 package org.jhonatan.pooclasesastractas.form;
 
 import org.jhonatan.pooclasesastractas.form.elementos.ElementoForm;
+import org.jhonatan.pooclasesastractas.form.elementos.InputForm;
+import org.jhonatan.pooclasesastractas.form.elementos.SelectForm;
+import org.jhonatan.pooclasesastractas.form.elementos.TextAreaForm;
+import org.jhonatan.pooclasesastractas.form.elementos.select.Opcion;
 
 public class EjemploForm {
     public static void main(String[] args) {
+        ejemploObjetos();
+    }
+
+    public static void ejemploObjetos() {
+        // creando un objeto de tipo InputForm
+        InputForm username = new InputForm("userename");
+        InputForm password = new InputForm("classe", "password");
+        InputForm email = new InputForm("email", "email");
+        InputForm edad = new InputForm("edad", "numbre");
+
+        // creando un objeto de tipo de textArea
+        TextAreaForm experencia = new TextAreaForm("exp", 5, 5);
+        SelectForm lenguaje = new SelectForm("lenguaje");
+
+        Opcion java = new Opcion("I", "Java");
+        lenguaje.addOpcion(java);
+        lenguaje.addOpcion(new Opcion("2", "Python"));
+        lenguaje.addOpcion(new Opcion("3", "C++"));
+        lenguaje.addOpcion(new Opcion("4", "JavaScript"));
+        lenguaje.addOpcion(new Opcion("5", "PHP"));
+
+        //establecemos valores
+        username.setValor("john.doe");
+        password.setValor("ab1c2d");
+        email.setValor("juan@gmail.com");
+        edad.setValor("45");
+        java.setSelected(true);
+        
+
+
+    }
+
+    public static void formaAnonima() {
         ElementoForm el = new ElementoForm() {
             @Override
             public String dibujarHtml() {
@@ -11,5 +48,4 @@ public class EjemploForm {
             }
         };
     }
-
 }
