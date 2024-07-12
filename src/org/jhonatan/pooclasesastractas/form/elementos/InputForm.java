855612c5c29fb119426cff3d1acf131c08c5d60a
jhonatan <ojedasnachezjhonatan@gmail.com>
2024-07-12 10:15:52 -0500

    clase InputForm: clase hija de ElementoForm poniendo sus atributos y método dibujarHtml()  de su clase padre

---
+++ src/org/jhonatan/pooclasesastractas/form/elementos/InputForm.java
@@ -0,0 +1,13 @@
+package org.jhonatan.pooclasesastractas.form.elementos;
+
+public class InputForm extends ElementoForm {
+
+    // atributos
+    private String tipo = "text";
+
+    @Override
+    public String dibujarHtml() {
+        return null;
+    }
+
+}
