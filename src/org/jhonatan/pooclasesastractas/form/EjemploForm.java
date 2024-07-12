3840ff0c02bf8cb0f9e33b1ffab9bbc7e5d9a94a
jhonatan <ojedasnachezjhonatan@gmail.com>
2024-07-12 11:29:10 -0500

    clase main EjemploForm : probando la aplicacion

--- src/org/jhonatan/pooclasesastractas/form/EjemploForm.java
+++ src/org/jhonatan/pooclasesastractas/form/EjemploForm.java
@@ -1,5 +1,8 @@
 package org.jhonatan.pooclasesastractas.form;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.jhonatan.pooclasesastractas.form.elementos.ElementoForm;
 import org.jhonatan.pooclasesastractas.form.elementos.InputForm;
 import org.jhonatan.pooclasesastractas.form.elementos.SelectForm;
@@ -29,15 +32,26 @@ public class EjemploForm {
         lenguaje.addOpcion(new Opcion("4", "JavaScript"));
         lenguaje.addOpcion(new Opcion("5", "PHP"));
 
-        //establecemos valores
+        // establecemos valores
         username.setValor("john.doe");
         password.setValor("ab1c2d");
         email.setValor("juan@gmail.com");
         edad.setValor("45");
+        experencia.setValor("...mas de 10 años de experencia...");
         java.setSelected(true);
-        
 
+        // creando una lista de la clase de elemento-Form
+        List<ElementoForm> elementoForms = new ArrayList<>();
+        elementoForms.add(username);
+        elementoForms.add(password);
+        elementoForms.addLast(email);
+        elementoForms.add(edad);
+        elementoForms.add(experencia);
+        elementoForms.add(lenguaje);
 
+        for (ElementoForm e : elementoForms) {
+            System.out.println(e.dibujarHtml());
+        }
     }
 
     public static void formaAnonima() {
