14a0d5829e6185ec44f822e72708745e027b3fd2
jhonatan <ojedasnachezjhonatan@gmail.com>
2024-07-12 10:19:40 -0500

    clase InputForm: constructores getters,setters

--- src/org/jhonatan/pooclasesastractas/form/elementos/InputForm.java
+++ src/org/jhonatan/pooclasesastractas/form/elementos/InputForm.java
@@ -5,6 +5,25 @@ public class InputForm extends ElementoForm {
     // atributos
     private String tipo = "text";
 
+    /* inicializa solo el objeto con el nombre */
+    public InputForm(String nombre) {
+        super(nombre);
+    }
+
+    /* inicializa con nombre y tipo el objeto creado */
+    public InputForm(String nombre, String tipo) {
+        super(nombre);
+        this.tipo = tipo;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
     @Override
     public String dibujarHtml() {
         return null;
