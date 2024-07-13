213a45f4f0249e7b79afd438ee8187792002b438
jhonatan <ojedasnachezjhonatan@gmail.com>
2024-07-13 12:03:55 -0500

    clase RequeridoValidador: clase hija de Validador sobre-escritura de metodos de la clase padre

---
+++ src/org/jhonatan/pooclasesastractas/form/validador/RequeridoValidador.java
@@ -0,0 +1,22 @@
+package org.jhonatan.pooclasesastractas.form.validador;
+
+public class RequeridoValidador extends Validador {
+
+    protected String mensaje = "el campo es requerido";
+
+    @Override
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    @Override
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+
+    @Override
+    public boolean esValido(String valor) {
+        return (valor != null && valor.length() > 0);
+    }
+
+}
