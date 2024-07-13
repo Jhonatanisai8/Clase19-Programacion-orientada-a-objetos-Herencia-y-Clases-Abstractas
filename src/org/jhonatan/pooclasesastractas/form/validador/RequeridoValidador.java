69563cd1397c36a0befacf6a5d58e991b1afc4c2
jhonatan <ojedasnachezjhonatan@gmail.com>
2024-07-13 13:50:56 -0500

    poniendo el %s para saber el nombre al cual estamos mostrando el error

--- src/org/jhonatan/pooclasesastractas/form/validador/RequeridoValidador.java
+++ src/org/jhonatan/pooclasesastractas/form/validador/RequeridoValidador.java
@@ -2,7 +2,7 @@ package org.jhonatan.pooclasesastractas.form.validador;
 
 public class RequeridoValidador extends Validador {
 
-    protected String mensaje = "el campo es requerido";
+    protected String mensaje = "el campo %s es requerido";
 
     @Override
     public String getMensaje() {
