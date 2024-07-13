eca9d3ee9bc774292d4af54f85db72b0154c28fe
jhonatan <ojedasnachezjhonatan@gmail.com>
2024-07-13 12:01:17 -0500

    clase NoNuloValidador: clae hija de la clase Validador sobre-escribiendo los metodos de la clase padre

---
+++ src/org/jhonatan/pooclasesastractas/form/validador/NoNuloValidador.java
@@ -0,0 +1,22 @@
+package org.jhonatan.pooclasesastractas.form.validador;
+
+public class NoNuloValidador extends Validador {
+
+    protected String mensaje = "el campo no puede ser nulo";
+
+    @Override
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+
+    @Override
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    @Override
+    public boolean esValido(String valor) {
+        return (valor != null);
+    }
+
+}
